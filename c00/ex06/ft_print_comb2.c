15e39c9ad5195427b982594b2626c3f8f745187a
Sharon nicole STOLL CABRERA <sstoll-c@e3r4p8.42.fr>
2021-07-14 18:02:47 +0200

    c00

---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sstoll-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/02 15:13:00 by sstoll-c          #+#    #+#             */
+/*   Updated: 2021/07/05 16:17:56 by sstoll-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_aff2(int a, int b)
+{
+	ft_putchar(a / 10 + '0');
+	ft_putchar(a % 10 + '0');
+	ft_putchar(' ');
+	ft_putchar(b / 10 + '0');
+	ft_putchar(b % 10 + '0');
+	if (a != 98)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}	
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_aff2(a, b);
+			b++;
+		}
+		a++;
+	}
+}
